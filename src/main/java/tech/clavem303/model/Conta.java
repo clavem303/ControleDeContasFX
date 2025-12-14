aba300952b6a8dbec5254fbd1c6de16b94688aed
clavem303 <clavem303@gmail.com>
2025-12-14 11:30:39 -0300

    feat(model): implementa esqueleto da classe abstrata conta

---
+++ src/main/java/tech/clavem303/model/Conta.java
@@ -0,0 +1,41 @@
+package tech.clavem303.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+abstract class Conta {
+
+    private String descricao;
+    private BigDecimal valorBase;
+    private LocalDate dataVencimento;
+    private boolean pago;
+
+    public Conta(String descricao, BigDecimal valorBase, LocalDate dataVencimento, boolean pago) {
+        this.descricao = descricao;
+        this.valorBase = valorBase;
+        this.dataVencimento = dataVencimento;
+        this.pago = pago;
+    }
+
+    public abstract double calcularValorTotal();
+
+    public void marcarComoPaga() {
+        this.pago = true;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public BigDecimal getValorBase() {
+        return valorBase;
+    }
+
+    public boolean isPago() {
+        return pago;
+    }
+}
