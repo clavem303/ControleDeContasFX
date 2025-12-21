d66b0e967042552bbc00f2505d7491ccc6dbae67
clavem303 <clavem303@gmail.com>
2025-12-21 20:25:07 -0300

    configura pom.xml para criar arquivo executável jar do projeto.

---
+++ src/main/java/tech/clavem303/Launcher.java
@@ -0,0 +1,8 @@
+package tech.clavem303;
+
+public class Launcher {
+    static void main(String[] args) {
+        // Apenas chama o main da sua classe App original
+        App.main(args);
+    }
+}
