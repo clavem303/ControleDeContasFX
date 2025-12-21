05e39ac294f83a86f84c6282dc4d83a7132185a8
clavem303 <clavem303@gmail.com>
2025-12-21 18:30:30 -0300

    cria classe app e testa dependências e plugin do javafx no maven via mvn javafx:run

--- src/main/java/tech/clavem303/App.java
+++ src/main/java/tech/clavem303/App.java
@@ -1,10 +1,21 @@
 package tech.clavem303;
 
-import tech.clavem303.view.ConsoleUI;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Olá, JavaFX com Maven!");
+        Scene scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     static void main() {
-        ConsoleUI ui = new ConsoleUI();
-        ui.iniciar();
+        launch();
     }
 }
